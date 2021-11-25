35fb5958b7c71d3769fc8399f062a8d4966e1750
Flavio Andrade <fandrade@imaginarycloud.com>
2021-11-25 10:45:00 +0000

    Add state to todo

--- packages/api/src/entities/todo.orm.entity.ts
+++ packages/api/src/entities/todo.orm.entity.ts
@@ -2,6 +2,13 @@ import { Entity, Column, ManyToOne, JoinColumn } from "typeorm";
 import { User } from "./user.orm.entity";
 import { EntityBase } from "../infrastructure/database/entity.base";
 
+export enum TodoStatus {
+  ACTIVE = "active",
+  DELETED = "deleted",
+  DONE = "done",
+  ARCHIVED = "archived",
+}
+
 @Entity()
 export class Todo extends EntityBase {
   constructor(props?: Partial<Todo>) {
@@ -17,6 +24,9 @@ export class Todo extends EntityBase {
 
   @Column()
   userId!: string;
+
+  @Column({ type: "enum", enum: TodoStatus, default: TodoStatus.ACTIVE })
+  status: TodoStatus = TodoStatus.ACTIVE;
 
   @ManyToOne(() => User, (user) => user)
   @JoinColumn({ name: "userId" })
